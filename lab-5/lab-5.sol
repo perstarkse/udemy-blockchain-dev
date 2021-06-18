fc2793ab6009cd8e95c90a2b813a8fa315883ced
perstarkse <perstark.se@gmail.com>
2021-06-18 13:02:41 +0200

    mappings pt 2
    updated to more recent solidity version and nested mapping

--- lab-5/lab-5.sol
+++ lab-5/lab-5.sol
@@ -1,8 +1,9 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.4;
 
 contract SimpleMappingExample {
     mapping(uint => bool) public myMapping;
     mapping(address => bool) public myAddressMapping;
+    mapping(uint => mapping(uint => bool)) uintUintBoolMapping;    
     
     function setValue(uint _index) public {
         myMapping[_index] = true;
@@ -10,5 +11,12 @@ contract SimpleMappingExample {
     
     function setMyAddresstoTrue() public {
         myAddressMapping[msg.sender] = true;
+    }
+    
+    function getuintUintBoolMapping(uint _index1, uint _index2) public view returns(bool){
+        return uintUintBoolMapping[_index1][_index2];
+    }
+    function setuintUintBoolMapping(uint _index1, uint _index2, bool _value) public {
+        uintUintBoolMapping[_index1][_index2] = _value;
     }
 }
